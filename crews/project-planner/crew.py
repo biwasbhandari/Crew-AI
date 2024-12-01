c6e446fd2c01fcbdb45cc7a5e72757656aff6b7e
Biwas Bhandari <biwas2059@gmail.com>
2024-12-01 19:24:59 +0545

    feat: add env example

--- crews/project-planner/crew.py
+++ crews/project-planner/crew.py
@@ -45,7 +45,7 @@ project_planning_agent = Agent(
     goal="To breakdown the {project_type} into actionable tasks, ensuring no detail is overlooked, and setting precise timelines that aligns with {project_objective}",
     backstory="As a veteran project planner you've succesfully led multiple projects, particularly in {industry}. Your keen eye for detail and strategic thinking have always ensured that projects are delivered on time and within scope. Now you're tasked with planning next groundbreaking {project_type} project.",
     allow_delegation=False,
-    verbose=True
+    verbose=False
 )
 
 estimation_agent = Agent(
@@ -53,7 +53,7 @@ estimation_agent = Agent(
     goal="Provide highly accurate time, resource, and effort estimations for each task in the {project_type} project to ensure it is delivered efficently and on budget.",
     backstory=" You are the go-to expert for project estimation in {industry}. With a wealth of expereience and access to vast historical data, you can predict the resources required for any task with remarkable accuracy.Your precision ensures that the {project_type} remains feasible and avoids unnecessary delays or budget overruns.",
     allow_delegation=False,
-    verbose=True
+    verbose=False
 )
 
 resource_allocation_agent = Agent(
@@ -61,7 +61,7 @@ resource_allocation_agent = Agent(
     goal=" Develop a resource allocation plan that precisely identifies and assigns resources to each task in the {project_type} project. Ensure optimal resource use to meet {project_objective} while maintaining efficiency and cost-effectiveness.",
     backstory="With extensive experience in {industry} and a track record in managing resource-heavy projects, you are adept at orchestrating the assignment of people, budget, and equipment to align with project requirements. Your goal is to balance the resource demands for the {project_type} to avoid bottlenecks and ensure seamless project progress. You work closely with the planning and estimation teams to confirm that all resource allocations are practical, feasible, and support the project's strategic objectives.",
     allow_delegation=False,
-    verbose=True
+    verbose=False
 )
 
 # CREATE TASKS
@@ -88,7 +88,7 @@ resource_allocation = Task(
 crew = Crew(
     agents=[project_planning_agent, estimation_agent, resource_allocation_agent],
     tasks=[task_breakdown, time_resource_estimation, resource_allocation],
-    verbose=True
+    # verbose=False
 )
 
 # ADD INPUTS FROM SUPABASE
@@ -111,3 +111,6 @@ inputs = {
 
 # KICK OFF THE CREW
 result = crew.kickoff(inputs=inputs)
+print(task_breakdown.output)
+print(time_resource_estimation.output)
+print(resource_allocation.output)
